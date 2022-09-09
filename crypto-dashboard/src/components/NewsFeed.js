af190933adf97667b2d98954dee87535dd9b58fa
haybales <haleynicole126@gmail.com>
2022-09-08 22:42:45 -0600

    apis

--- crypto-dashboard/src/components/NewsFeed.js
+++ crypto-dashboard/src/components/NewsFeed.js
@@ -1,7 +1,43 @@
+import {useEffect, useState} from 'react'
+import axios from 'axios'
+
 const NewsFeed = () => {
+  const[articles, setArticles] = useState(null)
+
+  useEffect(() => {
+    
+
+const options = {
+  method: 'GET',
+  url: 'https://crypto-news-live3.p.rapidapi.com/news',
+  headers: {
+    'X-RapidAPI-Key': 'f6199fa3dcmshfa46b350a69a920p191bf8jsncd19b81999e4',
+    'X-RapidAPI-Host': 'crypto-news-live3.p.rapidapi.com'
+  }
+};
+
+axios.request(options).then(function (response) {
+	console.log(response.data);
+  setArticles(response.data)
+}).catch(function (error) {
+	console.error(error);
+}, []);
+
+console.log(articles)
+
+
+
+  })
+  const first7Articles = articles?.slice(0,7)
+
+
     return (
       <div className="news-feed">
-        NewsFeed
+        <h2>NewsFeed</h2>
+        {first7Articles?.map((article, _index) => (
+        <div key={_index}>
+          <a href={article.url}><p>{article.title}</p></a>
+          </div>))}
       </div>
     )
   }
